0ce16db611598d546b5d1aa44c02342d57c5b28d
rzkyadhi <rizkyadhi081@gmail.com>
2022-07-19 12:37:15 +0700

    Latihan Javascript

---
+++ ConsumeMCC67API/wwwroot/js/latihan.js
@@ -0,0 +1,24 @@
+﻿document.getElementById("welcome").innerHTML = "Welcome To My Landing Page !";
+
+const collection = document.getElementsByClassName("name");
+collection[0].innerHTML = "My Name Is Rizky Adhi Nugroho";
+
+const favorite = document.getElementsByTagName("li");
+/*document.getElementById("favorite").innerHTML = favorite[0].innerHTML;*/
+
+document.addEventListener("click", getFavoriteFunction);
+
+document.querySelector(".list").style.backgroundColor = "red";
+const nodeList = document.querySelectorAll(".listSecond");
+for (let i = 0; i < nodeList.length; i++) {
+    nodeList[i].style.backgroundColor = "yellow";
+}
+
+function getFavoriteFunction() {
+    let result = document.getElementById("favorite").innerHTML = favorite[1].innerHTML;
+    return result;
+}
+
+function leastFavorite() {
+    document.getElementById("leastFavorite").innerHTML = favorite[0].innerHTML;
+}
