2ecbe51f7ea2aa90d6949c6c365a06f91a8644a1
rzkyadhi <rizkyadhi081@gmail.com>
2022-07-21 09:23:29 +0700

    Adding Star Wars Controller and Consume GET

---
+++ ConsumeMCC67API/Controllers/StarWarsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ConsumeMCC67API.Controllers
+{
+    public class StarWarsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ConsumeMCC67API/wwwroot/js/starwars.js
@@ -0,0 +1,21 @@
+﻿$.ajax({
+	url: "https://swapi.dev/api/people/"
+}).done((result) => {
+	let text = "";
+	$.each(result.results, function (key, val) {
+		text +=
+			`<tr>
+			<td>${key+1}</td>
+			<td>${val.name}</td>
+			<td>${val.height}</td>
+			<td>${val.mass}</td>
+			<td>${val.gender}</td>
+			<td>${val.birth_year}</td>
+			<td>
+				<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#detailsModal">
+  					Details
+				</button></td>
+		</tr>`;
+	});
+	$("#starWarsData").html(text);
+})
