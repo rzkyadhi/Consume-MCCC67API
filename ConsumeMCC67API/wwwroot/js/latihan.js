4b04b251fd5d76b28a32031687a83bc6027d46b4
rzkyadhi <rizkyadhi081@gmail.com>
2022-07-21 07:31:17 +0700

    latihan css js day 4

--- ConsumeMCC67API/wwwroot/js/latihan.js
+++ ConsumeMCC67API/wwwroot/js/latihan.js
@@ -1,68 +1,100 @@
-﻿function changeText() {
-    var text2 = document.querySelectorAll(".container .row .col")[1];
-    text2.innerHTML = "Changed !";
-    return text2;
-}
+﻿// function changeText() {
+//     var text2 = document.querySelectorAll(".container .row .col")[0];
+//     text2.innerHTML = "Changed !";
+//     return text2;
+// }
 
-let mouseOver = document.querySelectorAll(".container .row .col");
+// let mouseOver = document.querySelectorAll(".container .row .col");
 
-for (let i = 0; i < mouseOver.length; i++) {
-    mouseOver[i].addEventListener("mouseover", function (event) {
-        event.target.style.color = "orange";
+// for (let i = 0; i < mouseOver.length; i++) {
+//     mouseOver[i].addEventListener("mouseover", function (event) {
+//         event.target.style.color = "orange";
 
-        setTimeout(function () {
-            event.target.style.color = "";
-        }, 500);
-    }, false);
-}
+//         setTimeout(function () {
+//             event.target.style.color = "";
+//         }, 500);
+//     }, false);
+// }
 
-// Fundamentals
-const animals = [{
-        name: "Dory",
-        species: "fish",
-        class: {
-            name: "invertebrata"
-        }
-    },
-    {
-        name: "Garfield",
-        species: "cat",
-        class: {
-            name: "mamalia"
-        }
-    },
-    {
-        name: "Budi",
-        species: "cat",
-        class: {
-            name: "mamalia"
-        }
-    },
-    {
-        name: "Nemo",
-        species: "fish",
-        class: {
-            name: "invertebrata"
-        }
-    },
-    {
-        name: "Tom",
-        species: "cat",
-        class: {
-            name: "mamalia"
-        }
-    }
-]
+// // Fundamentals
+// const animals = [{
+//         name: "Dory",
+//         species: "fish",
+//         class: {
+//             name: "invertebrata"
+//         }
+//     },
+//     {
+//         name: "Garfield",
+//         species: "cat",
+//         class: {
+//             name: "mamalia"
+//         }
+//     },
+//     {
+//         name: "Budi",
+//         species: "cat",
+//         class: {
+//             name: "mamalia"
+//         }
+//     },
+//     {
+//         name: "Nemo",
+//         species: "fish",
+//         class: {
+//             name: "invertebrata"
+//         }
+//     },
+//     {
+//         name: "Tom",
+//         species: "cat",
+//         class: {
+//             name: "mamalia"
+//         }
+//     }
+// ]
 
-const onlyCat = [];
-const vertebrata = []
-for (let i = 0; i < animals.length; i++) {
-    if (animals[i].species == "cat") {
-        onlyCat.push(animals[i]);
-    }
+// let onlyCat = [];
+// let vertebrata = [];
+// // for (let i = 0; i < animals.length; i++) {
+// //     if (animals[i].species == "cat") {
+// //         onlyCat.push(animals[i]);
+// //     }
 
-    if (animals[i].class.name == "invertebrata") {
-        animals[i].class.name = "Non Mamalia";
-        vertebrata.push(animals[i]);
-    }
-}
+// //     if (animals[i].class.name == "invertebrata") {
+// //         animals[i].class.name = "Non Mamalia";
+// //         vertebrata.push(animals[i]);
+// //     }
+// // }
+
+// onlyCat = animals.filter(x => x.species == "cat");
+// console.log(onlyCat);
+// vertebrata = animals.filter(x => x.class.name == "invertebrata");
+// for (let i = 0; i < vertebrata.length; i++){
+//     vertebrata[i].class.name = "Non Mamalia";
+// }
+// console.log(vertebrata);
+
+// var catz = [];
+// var cats = catz;
+// catz = animals.filter(x => x.species == "cat");
+// cats = animals.filter(x => x.species == "fish");
+
+// AJAX
+$.ajax({
+    url: "https://pokeapi.co/api/v2/pokemon/"
+}).done((result) => {
+    console.log(result.results);
+    $.each(result.results, function (key, val) {
+        console.log(val.name);
+    })
+});
+
+$.ajax({
+    url: "https://pokeapi.co/api/v2/pokemon/"
+}).done((result) => {
+    let text = "";
+    $.each(result.results, function (key, val) {
+        text += `<tr></tr>`
+    })
+});
