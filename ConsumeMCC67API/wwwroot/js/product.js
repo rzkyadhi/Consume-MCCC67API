fa21135bb3357d06994b3fdba7b28d34a5c02efa
rzkyadhi <rizkyadhi081@gmail.com>
2022-07-26 14:28:58 +0700

    Implement Export Button on Product Controller(GET) ON CLIENT

--- ConsumeMCC67API/wwwroot/js/product.js
+++ ConsumeMCC67API/wwwroot/js/product.js
@@ -1,8 +1,69 @@
 ﻿$(document).ready(function () {
-    $('#tableProduct').DataTable({
-        columnDefs: [
-            { orderable: false, targets: -1 },
-            { className: 'text-center', targets: [0, 1, 2, 3] }
+    var table = $('#tableProduct').DataTable({
+        lengthChange: false,
+        "initComplete": function () {
+            table.buttons().container().appendTo('#tableProduct_wrapper .col-md-6:eq(0)');
+            $("#tableProduct").show();
+        },
+        /*dom: 'Bfrtip',*/
+        lengthMenu: [
+            [10, 25, 50, -1],
+            ['10 rows', '25 rows', '50 rows', 'Show all']
+        ],
+        buttons: [
+            {
+                extend: 'pageLength',
+                className: 'btn btn-primary dropdown-toggle rounded'
+            },
+            'spacer',
+            {
+                extend: 'collection',   
+                text: 'Export As',
+                className: 'btn btn-primary rounded',
+                buttons: [
+                    {
+                        extend: 'copy',
+                    },
+                    {
+                        extend: 'pdf',
+                        title: 'DataExportPDF - ProductMCC67',
+                        exportOptions: {
+                            columns: ':visible'
+                        },
+                        download: 'open'
+                    },
+                    {
+                        extend: 'excel',
+                        title: 'DataExportExcel - ProductMCC67',
+                        exportOptions: {
+                            columns: ':visible'
+                        },
+                        autoFilter: true,
+                        sheetName: 'ProductMCC67'
+                    },
+                    {
+                        extend: 'csv',
+                        title: 'DataExportCSV - ProductMCC67',
+                        exportOptions: {
+                            columns: ':visible'
+                        }
+                    }
+                ]
+            },
+            'spacer',
+            {
+                extend: 'colvis',
+                className: 'btn btn-primary rounded',
+            }
+        ],
+        columnDefs: [{
+                orderable: false,
+                targets: -1
+            },
+            {
+                className: 'text-center',
+                targets: [0, 1, 2, 3]
+            }
         ],
         "ajax": {
             "url": "https://localhost:44313/api/product",
@@ -26,12 +87,12 @@
                 render: function (data, type, row) {
 
                     return `
-                    <a href="https://localhost:44317/product/edit/${row['id']}" class="btn btn-warning">
-                        Edit
-                    </a>
-                    <a type="button" href="https://localhost:44317/product/delete/${row['id']}" class="btn btn-danger">
-                        Delete
-                    </a>`
+                                <a href="https://localhost:44317/product/edit/${row['id']}" class="btn btn-warning">
+                                    Edit
+                                </a>
+                                <a type="button" href="https://localhost:44317/product/delete/${row['id']}" class="btn btn-danger">
+                                    Delete
+                                </a>`
                 }
             }
         ]
