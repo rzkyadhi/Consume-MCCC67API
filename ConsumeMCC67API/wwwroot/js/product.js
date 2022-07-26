3a5a2964ca3a09c1d07548ffa05b0c1cdf1721bf
rzkyadhi <rizkyadhi081@gmail.com>
2022-07-26 07:33:44 +0700

    Implement Data Table on Product Controller(GET) ON CLIENT

---
+++ ConsumeMCC67API/wwwroot/js/product.js
@@ -0,0 +1,39 @@
+﻿$(document).ready(function () {
+    $('#tableProduct').DataTable({
+        columnDefs: [
+            { orderable: false, targets: -1 },
+            { className: 'text-center', targets: [0, 1, 2, 3] }
+        ],
+        "ajax": {
+            "url": "https://localhost:44313/api/product",
+            "dataType": "json",
+            "dataSrc": "data",
+        },
+        "columns": [{
+                "data": "id",
+                render: function (data, type, row, meta) {
+                    return meta.row + meta.settings._iDisplayStart + 1;
+                }
+            },
+            {
+                "data": "name"
+            },
+            {
+                "data": "supplier.name"
+            },
+            {
+                // data: null,
+                render: function (data, type, row) {
+
+                    return `
+                    <a href="https://localhost:44317/product/edit/${row['id']}" class="btn btn-warning">
+                        Edit
+                    </a>
+                    <a type="button" href="https://localhost:44317/product/delete/${row['id']}" class="btn btn-danger">
+                        Delete
+                    </a>`
+                }
+            }
+        ]
+    });
+});
